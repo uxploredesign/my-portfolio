d87f0749bdd4f35b91c773e6a361e7b6692fe1de
uxploredesign <167592073+uxploredesign@users.noreply.github.com>
2024-04-25 07:37:03 +0200

    add code for mobile hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-console.log("Welcome to My Portfolio")
+function menuToggle() {
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+        x.className += " responsive";
+    } else {
+        x.className = "navtoggle";
+    }
+}
+
+function adjustNavItemsWidth() {
+    var navItems = document.querySelectorAll('.navtoggle nav li');
+    var maxWidth = 0;
+  
+    // Find the width of the widest item
+    navItems.forEach(function(item) {
+      if(item.offsetWidth > maxWidth) {
+        maxWidth = item.offsetWidth;
+      }
+    });
+  
+    // Set all items to that width
+    navItems.forEach(function(item) {
+      item.style.width = maxWidth + 'px';
+    });
+  }
+  
+  // Adjust the widths when the page loads
+  window.onload = adjustNavItemsWidth;
